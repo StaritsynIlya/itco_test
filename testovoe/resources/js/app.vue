b40d1197d7645982aa0339dc5b46720a78a68402
StaritsynIlya <vladokc33@gmail.com>
2023-09-05 21:23:13 +0300

    Решил разбираться с ларавель Сделал подобие бд, нужно разбираться с API и как здесь настроить vue.js

---
+++ testovoe/resources/js/app.vue
@@ -0,0 +1,14 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import VueRouter from 'vue-router';
+
+window.Vue.use(VueRouter);
+
+import ProjectsIndex from './components/projects/ProjectsIndex.vue';
+import ProjectsCreate from './components/projects/ProjectsCreate.vue';
+import ProjectsEdit from './components/projects/ProjectsEdit.vue';
+
+const router = new VueRouter({ routes })
+
+const app = new Vue({ router }).$mount('#app')
---
+++ testovoe/resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ItCo - тестовое задание</title>
+
+</head>
+
+<body>
+    <p>Coming soon...</p>
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Project</div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
+</body>
+
+</html>
